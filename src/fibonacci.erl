c8cf09f4a52d0238a7200bd6c24eb1231084ba5c
liwnd <547319765@qq.com>
2020-02-19 14:06:45 +0800

    nothing1

--- src/fibonacci.erl
+++ src/fibonacci.erl
@@ -6,14 +6,11 @@
 %%% @end
 %%% Created : 23. 十二月 2019 12:23
 %%%-------------------------------------------------------------------
--module(doc).
+-module(fibonacci).
 -author("10990").
 
 %% API
--export([feibo_list/1,ele/1]).
-
-%% 运行：feibo:feibo_list(5).
-%% 结果示例：【1,1,2,3,5】
+-export([fibonacci_list/1,ele/1]).
 
 %% 函数element主要为了计算斐波那契数列的第N个元素
 ele(1) -> 1;
@@ -21,6 +18,6 @@ ele(2) -> 1;
 ele(N) -> ele(N-1) + ele(N-2).
 
 %% 给定一个N，求出斐波那契的前N个数
-feibo_list(N) -> feibo_list([], N).
-feibo_list(L, 0) -> L;
-feibo_list(L, N) -> feibo_list([ele(N)|L], N-1).
+fibonacci_list(N) -> fibonacci_list([], N).
+fibonacci_list(L, 0) -> L;
+fibonacci_list(L, N) -> fibonacci_list([ele(N)|L], N-1).
