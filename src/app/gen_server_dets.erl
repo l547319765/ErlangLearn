f8f2880a93fcaae8905033b559aadcdebc0d810c
liwnd <547319765@qq.com>
2020-03-19 23:52:12 +0800

    任务3 完成

--- src/app/gen_server_dets.erl
+++ src/app/gen_server_dets.erl
@@ -4,14 +4,14 @@
 %%% @doc
 %%%
 %%% @end
-%%% Created : 17. 三月 2020 17:41
+%%% Created : 19. 三月 2020 19:17
 %%%-------------------------------------------------------------------
--module(gen_server_demo).
+-module(gen_server_dets).
 -author("10990").
 -behavior(gen_server).
 -compile(export_all).
 %% API
--export([start/1,add_task/2,get_task/0,cencel_task/0,task_center/0,add_to_list/3,pull_task/1]).
+-export([start/1,add_task/2,get_task/0,cencel_task/0,task_center/0,get_dets/0,center_init/1,add_to_list/3,pull_task/1]).
 %%回调函数
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -define(SERVER,?MODULE).
@@ -20,24 +20,25 @@
   task_center ,      %%任务中心
   task = none,      %%当前执行任务
   length = 0,        %%任务队列长度
-  task_list = []     %%任务队列
+  task_list     %%任务队列
 }).
 
 %%        test
-%%        gen_server_demo:start(task_center).
-%%        gen_server_demo:add_task(job1,15000).
-%%        gen_server_demo:add_task(job2,15000).
-%%        gen_server_demo:add_task(job3,15000).
-%%        gen_server_demo:cencel_task().
-%%        gen_server_demo:get_task().
+%%        gen_server_dets:start(task_center).
+%%        gen_server_dets:add_task(job1,15000).
+%%        gen_server_dets:add_task(job2,15000).
+%%        gen_server_dets:add_task(job3,15000).
+%%        gen_server_dets:cencel_task().
+%%        gen_server_dets:get_task().
+%%        gen_server_dets:get_dets().
 
 
 %%-------------gen_server----------API
 init([Name]) ->
-  State = #state{},
-  State2 = State#state{task_center = Name},
+  TaskList =  center_init(task_list),
+  State =#state{task_center = Name,task_list = TaskList},
   register(Name,spawn(fun()-> task_center()end)),
-  {ok,State2}.
+  {ok,State}.
 
 handle_call({add_task,Name,Time},_From,State) ->
 %%  获取当前是否在运行任务，不是的话启动任务，不然就添加到队列里。
@@ -80,13 +81,15 @@ handle_call({unname_task},_From,State) ->
   Test_Center ! {start_task,State2},
   {reply, Reply, State2};
 
-handle_call({name_task},_From,State) ->
-  {Result,Name,State_new} = pull_task(State),
-%%  io:format("new_running~p",[Name]),
-  Name2 = State_new#state.task,
-%%  io:format("new_running~p",[Name2]),
-  Reply = Name2,
-  {reply, Reply, State_new}.
+handle_call({name_task,Name},_From,State) ->
+  Len = State#state.length,
+  Reply = Name,
+  {reply, Reply, State#state{task = Name,length = Len-1}};
+
+handle_call({get_dets},_From,State) ->
+  TaskList = State#state.task_list,
+  Reply = TaskList,
+  {reply, Reply, State}.
 
 handle_cast(_Msg,State)-> {noreply,State}.
 
@@ -113,22 +116,41 @@ get_task()->
 %%3.取消正在执行任务
 cencel_task()->
   gen_server:call(?MODULE,{cencel_task}).
-%%4.停止任务管理中心
-%%stop()-> call(?MODULE,stop).
+
+%%4.进程外获取dets的name
+get_dets()->
+  gen_server:call(?MODULE,{get_dets}).
 %%------------task_list------API
 %%初始化task_list
-%%center_init(State = #state{}) -> {State}.
+center_init(Name) ->
+  %%  使用类似数据库自增主键的方式记录到了哪个数据,用另一个数据记录任务队列使用了多少
+  File = "./Data.dets",
+  io:format("dets opened:~p~n", [File]),
+  %%  如果是文件，证明已经有过这个文件了，所以直接修复打开，否则新插入index。
+  Bool = filelib:is_file(File),
+  case dets:open_file(Name, [{file, File}]) of
+    {ok, TaskList} ->
+      case Bool of
+        true  ->void;
+        false ->ok = dets:insert(TaskList, {index,0,0})
+      end,
+      TaskList;
+    {error, _Reason} ->
+      io:format("cannot open dets table~n"),
+      exit(eDetsOpen)
+  end.
 
 %%添加入task_list
 add_to_list(Name,Time,State)->
   Len  =  State#state.length,
-  Buff =  State#state.task_list,
-  BBuff = Buff ++ [{Name,Time}],
-  State#state{length = Len+1, task_list = BBuff}.
+  TaskList = State#state.task_list,
+  [{index,Data_Index,Job_Index}] = dets:lookup(TaskList,index),
+  dets:insert(TaskList,[{index,Data_Index+1,Job_Index},{Data_Index,Name,Time}]),
+  State#state{length = Len+1}.
 
 %%拉取首部的任务
 pull_task(State)->
-  Buff = State#state.task_list,
+  TaskList = State#state.task_list,
   Len = State#state.length,
   No_task = none,
   Reply =
@@ -136,10 +158,10 @@ pull_task(State)->
       0  ->
         {false,none,State#state{task = No_task}};
       _ ->
-        T = hd(Buff),
-        BBuff = tl(Buff),
-        {Name,Time}= T,
-        {Time,Name,State#state{task = Name, length = Len-1, task_list = BBuff}}
+        [{index,Data_Index,Job_Index}] = dets:lookup(TaskList,index),
+        [{_,Name,Time}] = dets:lookup(TaskList,Job_Index),
+        dets:insert(TaskList,[{index,Data_Index,Job_Index+1}]),
+        {Time,Name,State#state{task = Name, length = Len-1}}
     end,
   Reply.
 
@@ -152,7 +174,7 @@ task_center()->
         false -> io:format("don't have task"),
           task_center();
         _ ->
-          gen_server:call(?MODULE,{name_task}),
+          gen_server:call(?MODULE,{name_task,Name}),
           receive
             {cencel_task} ->
               io:format("cencel_runing_task"),
